d03d4debdf1dfac8d24fa2ded6f0dbeed30879d3
softboy <zzu_softboy@163.com>
2017-10-29 15:11:54 +0800

    finished hexo php apidoc generator

---
+++ index.js
@@ -0,0 +1,81 @@
+"use strict";
+/**
+ * @author ZZU_SOFTBOY
+ */
+let Promise = require("bluebird");
+let _ = require('lodash');
+let fs = require('fs');
+let Git = require("nodegit");
+const { execSync,spawn } = require('child_process');
+
+let generatePhpApiDocs = !!hexo.env.args.withPhpApidoc;
+
+
+
+if (generatePhpApiDocs) {
+   hexo.extend.filter.register("before_generate", function() {
+      let get_php_apidoc_saved_dir = hexo.extend.helper.get("get_php_apidoc_saved_dir");
+      let get_temp_dir = hexo.extend.helper.get("get_temp_dir");
+      let get_root_dir = hexo.extend.helper.get("get_root_dir");
+      let tempDir = get_temp_dir();
+      let ideAutoCompleteDir = tempDir+'/ide-auto-complete';
+      let apigenExec = get_root_dir()+"/tools/apigen/bin/apigen";
+      return new Promise(function (resolve, reject)
+      {
+         if (!fs.existsSync(ideAutoCompleteDir)) {
+            return Git.Clone("https://github.com/libpdk/ide-auto-complete.git", ideAutoCompleteDir).then(function(repository){
+                  resolve(ideAutoCompleteDir);
+            });
+         }else {
+            var repository;
+           
+            // first reset HEAD
+            return Git.Repository.open(ideAutoCompleteDir).then(function(repo) {
+               // Use repository
+               repository = repo;
+               return repository.fetchAll({
+                  callbacks: {
+                     credentials: function(url, userName) {
+                        return Git.Cred.sshKeyFromAgent(userName);
+                     },
+                     certificateCheck: function() {
+                        return 1;
+                     }
+                  }
+               });
+            }).then(function(){
+               return repository.mergeBranches("master", "origin/master");
+            }).then(function(){
+               resolve(ideAutoCompleteDir);
+            }).catch(function(error){
+               console.log(error);
+               reject();
+            });
+         }
+      }).then(function(repoDir) {
+         return new Promise(function (resovle, reject)
+         {
+            // Work with the repository object here.
+            const ls = spawn('php', [apigenExec, "generate", "src", "--destination", get_php_apidoc_saved_dir()], {
+               cwd:ideAutoCompleteDir
+            });
+            ls.stdout.on('data', (data) => {
+               console.log(`stdout: ${data}`);
+            });
+            ls.stderr.on('data', (data) => {
+               console.log(`stderr: ${data}`);
+            });
+            ls.on('close', (code) => {
+               if (0 !== code) {
+                  console.log("apigen error");
+                  reject("apigen error");
+               } else {
+                  resovle();
+               }
+            });
+         });
+      }).catch(function(error){
+         console.log(error);
+      });
+   }, 1);
+}
